46ae089cbf3e739847ff9f18c796d1769fadcc09
moualhi zine el abidine <zmoualhi@outlook.com>
2023-11-19 12:31:44 +0100

    fix 🐛: fixed typo in completion script

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -3,5 +3,5 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
-  go run main.go completion "$sh" >"completions/goji.$sh"
+  go run main.go completion "$sh" >"completions/cloudcost.$sh"
 done
